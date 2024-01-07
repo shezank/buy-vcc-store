58bae3a0ecc352d96d3dc3f784101cad4dbca46a
shezank <mdfarhankobirkhan1@gmail.com>
2024-01-07 17:11:58 +0600

    login style update

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -52,13 +52,12 @@ const Login = () => {
         }
     }
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero min-h-screen bg-teal-100">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm border-4">
                     <form onSubmit={handleLogin} className="card-body">
                         <div className="text-center lg:text-left">
                             <h1 className="text-5xl font-bold">Login now!</h1>
-
                         </div>
                         <div className="form-control">
                             <label className="label">
